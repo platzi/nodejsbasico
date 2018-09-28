858371c8647788a139a013af68bd6f24b261c28d
Emir Salazar <emiralexisl@gmail.com>
2018-09-27 23:38:03 -0500

    Creando base para la clase.

---
+++ src/call.js
@@ -0,0 +1,15 @@
+module.exports = {
+    greet: function(nombreCompleto) {
+        console.log(`Hola ${nombreCompleto}! Sientes la asincronia?`);
+    },
+    withPromise: function(nombre, apellido) {
+        console.log("Llamando función con promesa...");
+        return new Promise((resolve, reject)=>{
+            console.log("Ejecutando espera...");
+            setTimeout(()=>{
+                console.log("Resolviendo la espera!");
+                resolve(`${nombre} ${apellido}`);
+            }, 5000); 
+        })
+    }
+}
