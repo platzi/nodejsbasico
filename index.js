e1bcc36a32746f1a90a32a9d6fc71c2fccdeb15f
Emir Salazar <emiralexisl@gmail.com>
2018-10-02 01:56:58 -0500

    Creando base para la clase.

---
+++ index.js
@@ -0,0 +1,13 @@
+function a() {
+    console.log("Estoy imprimiendo la llamada a la función A.");
+}
+
+function b() {
+    console.log("Estoy imprimiendo la llamada a la función B.");
+}
+
+function c() {
+    console.log("Estoy imprimiendo la llamada a la función C.");
+}
+
+a(); b(); c();
