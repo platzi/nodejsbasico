a6bca8df8548ca2d3a0deafd88ec6cb1759ef185
Emir Salazar <emiralexisl@gmail.com>
2018-10-05 05:08:29 -0500

    Creando version final de la clase.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require("http").Server();
+const io = require("socket.io")(server);
+const port = require("./config").SERVER_PORT;
+
+const banner = `
+****************************
+    Basic Node.js Course
+       Course Project
+     Tic-Tac-Toe Server
+****************************
+Status: Online
+Listening on port ${port}
+`;
+
+io.on("connection", (socket) => {
+    socket.on("register", (user) => {
+        console.info(`User registered: {name: ${user.name}, id: ${user.id}}`)
+    });
+});
+
+server.listen(port, () => {
+    console.info(banner);
+});
